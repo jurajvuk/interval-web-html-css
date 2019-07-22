5de09249700caa1deaaf264100bcf3e5c33fe1f2
jurajvuk <jurajvuk@gmail.com>
2019-07-22 15:54:29 +0200

    dodan image slider

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Navigacija, animacije
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
@@ -13,12 +14,54 @@ const navSlide = () => {
             } else {
                 link.style.animation = 'navLinkFade 0.5s ease forwards ' + (index / 7 + 0.5) + 's';
             }
-            
         });
         burger.classList.toggle('toggle');
     });
-
-    
 }
 
 navSlide();
+
+// Image slider, animacije
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImage = document.querySelectorAll('.carousel-slide img');
+
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn'); 
+
+// Counter
+let counter = 1;
+const size = carouselImage[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+// Event listeners for buttons
+nextBtn.addEventListener('click', () => {
+    if (counter >= carouselImage.length - 1)
+        return;
+    carouselSlide.style.transition = "transform 1s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+// Event listeners for buttons
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0)
+        return;
+    carouselSlide.style.transition = "transform 1s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitionend', () => {
+    if (carouselImage[counter].id === "lastClone") {
+        carouselSlide.style.transition = "none";
+        counter = carouselImage.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if (carouselImage[counter].id === "firstClone") {
+        carouselSlide.style.transition = "none";
+        counter = carouselImage.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+});
